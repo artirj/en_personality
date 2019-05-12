c104eeb730c78dc12e77710115f299f2d402f6c3
Jose Luis Ricon <jose@ricon.xyz>
2019-05-12 07:13:13 +0100

    cleaning code

---
+++ clean.py
@@ -0,0 +1,75 @@
+from load import load_sql
+
+# Cleaning
+def no_forward(df):
+    return df[~df.body.str.startswith("---------------------- Forwarded by")]
+
+
+def no_original_msg(df):
+    df = df.copy()
+    exprs = [
+        "-----Original Message",
+        "---------------------- Forwarded by",
+        "----- Forwarded by",
+        "-------- Inline attachment follows",
+        "\t\t\tFrom:",
+        "cc: Subject:",
+        "Sara ShackletonEnron North America Corp",
+        " Debra PerlingiereEnron",
+        "Outlook Migration Team",
+        "Matthew B Fleming@",
+        "Lynn Lynn Blair Northern",
+        "TASK ASSIGNMENT",
+        "X-FileName",
+        "KayRhonda L Denton@ECT0",
+    ]
+    # Find emails to drop
+    drop_emails = (
+        df.body.str.extract('("[A-z \.,]+" <[a-zA-Z0-9_.]+@[A-z ]+\..{2,3}>)')[0]
+        .dropna()
+        .unique()
+        .tolist()
+    )
+    exprs += drop_emails
+    drop_emails2 = (
+        df.body.str.extract("((?:From:)? [A-z ]+@[A-Z]{1,7})").dropna()[0].unique().tolist()
+    )
+    exprs += drop_emails2
+    drop_emails3 = (
+        df.body.str.extract("([0-9]{2}/[0-9]{2}/[0-9]{4} [0-9]{2}:[0-9]{2} PMTo:)")
+        .dropna()[0]
+        .unique()
+        .tolist()
+    )
+    exprs += drop_emails3
+    for expr in exprs:
+        mask = df.body.str.contains(expr, regex=False)
+
+        df.loc[mask, "body"] = df.loc[mask, "body"].apply(lambda x: x.split(expr)[0])
+    return df
+
+
+def remove_words(df):
+    df = df.copy()
+    reg = {
+        "--------------------------Sent from my BlackBerry Wireless Handheld \(www\.BlackBerry\.net\)": "",
+        "\t": " ",
+        " +": " ",
+    }
+    for exp, replace in reg.items():
+        df["body"] = df.body.str.replace(exp, replace, regex=True)
+    return df
+
+
+def remove_blank(df):
+    df["body"] = df["body"].str.strip()
+    return df[df.body.str.len() >= 2]
+
+
+def process_df(df):
+    return df.pipe(no_forward).pipe(no_original_msg).pipe(remove_words).pipe(remove_blank)
+
+
+def load_clean_data(min_sender=100):
+    data = load_sql(min_sender)
+    return data.pipe(process_df)
--- load.py
+++ load.py
@@ -2,15 +2,24 @@ import pandas as pd
 
 
 def load_sql(min_sender=100):
+    """
+    We load only frequent messages to have a sufficient corpus per employee.
+    Also, load only employees (that exist in employee tables)
+    Not corporate accounts (comms, etc)
+    """
     con = "mysql+mysqldb://root:@127.0.0.1:" f"3306/enron"
     msg = pd.read_sql_query(
-        f"""SELECT sender, body FROM  message
-    WHERE sender IN(
-    SELECT DISTINCT sender
-    FROM message
-    GROUP BY sender
-    HAVING COUNT(*)>{min_sender}
-    )""",
+        f"""SELECT sender,body FROM
+        (SELECT sender, body FROM  message
+        WHERE sender IN(
+        SELECT DISTINCT sender
+        FROM message
+        GROUP BY sender
+        HAVING COUNT(*)>{min_sender}
+        ) ) AS tab
+        INNER JOIN
+        employeelist
+        ON employeelist.Email_id=tab.sender""",
         con,
     )
     return msg
